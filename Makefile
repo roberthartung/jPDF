5e27997ecedfe39c82d184850be3da43c57139f0
Robert Hartung <robert-hartung@web.de>
2014-06-05 17:49:33 +0200

    doc: Makefile + ignore

---
+++ Makefile
@@ -0,0 +1,2 @@
+doc:
+	javadoc -d doc src/jpdf/*.java src/jpdf/*/*.java
--- src/jpdf/Document.java
+++ src/jpdf/Document.java
@@ -4,11 +4,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import jpdf.parser.AbstractParser;
+import jpdf.parser.Parser;
 import jpdf.parser.ParserException;
 
 public class Document {
+	private Parser parser;
+
+	public Document(Parser p) {
+		this.parser = p;
+	}
+	
 	public static Document fromFile(String file) throws FileNotFoundException, ParserException {
 		AbstractParser p = new AbstractParser(new File(file));
 		return p.parse();
+	}
+	
+	public int getPageCount() {
+		return 0;
 	}
 }
